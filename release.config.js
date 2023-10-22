1f3f0dbe0710e7877fa87fc71fa841bf0bf016ea
Wells <ll0918900@gmail.com>
2023-10-22 10:44:05 +0800

    chore: add release.config.js

---
+++ release.config.js
@@ -0,0 +1,24 @@
+﻿module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/npm',{npmPublish: false}],
+        [
+            '@semantic-release/changelog',
+            {
+                mangle: false,
+                headerIds: false,
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['CHANGELOG.md', 'package.json'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]',
+            },
+        ],
+    ],
+};
